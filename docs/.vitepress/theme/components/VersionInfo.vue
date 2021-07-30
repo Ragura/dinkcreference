ce81879dd6949571ba2566e309aa5c4245e59aba
Steven Schoovaerts <ragura@gmail.com>
2021-07-30 20:03:48 +0200

    style: restyle VersionInfo component

--- docs/.vitepress/theme/components/VersionInfo.vue
+++ docs/.vitepress/theme/components/VersionInfo.vue
@@ -7,25 +7,44 @@
 </script>
 
 <template>
-    <span v-if="props.dink" class="version dink" title="Dink version">
-        {{ props.dink }}
-    </span>
-    <span v-if="props.freedink" class="version freedink" title="FreeDink version">
-        {{ props.freedink }}
-    </span>
-    <span v-if="props.dinkhd" class="version dinkhd" title="DinkHD version">
-        {{ props.dinkhd }}
-    </span>
-    <slot></slot>
+    <div class="version dink" v-if="props.dink" title="Dink">
+        <div class="title">
+            Dink
+        </div>
+        <div class="number">
+            {{ props.dink }}
+        </div>
+    </div>
+    <div class="version freedink" v-if="props.freedink" title="Freedink">
+        <div class="title">
+            Freedink
+        </div>
+        <div class="number">
+            {{ props.freedink }}
+        </div>
+    </div>
+    <div class="version dinkhd" v-if="props.dinkhd" title="DinkHD">
+        <div class="title">
+            DinkHD
+        </div>
+        <div class="number">
+            {{ props.dinkhd }}
+        </div>
+    </div>
+    <slot />
 </template>
 
 <style scoped>
     .version {
         display: inline-block;
-        padding: 0.25em 0.5em;
+        padding: 0.20em 0.5em 0.35em 0.5em;
         color: white;
         border-radius: 0.25em;
-        margin-right:0.25em;
+        margin:0.25em 0.3em 0em 0em;
+        line-height: 0.9em;
+        font-size: 0.75em;
+        text-align: center;
+        font-weight: bold;
     }
 
     .dink {
@@ -36,5 +55,9 @@
     }
     .dinkhd {
         background-color: #FF6B6B;
+    }
+
+    .title {
+        font-size: 0.6em;
     }
 </style>
